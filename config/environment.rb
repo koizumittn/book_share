b0602b9a17b68de658a4a9ac9c4114c9dc09b2ca
tech-camp <tech-camp@tech-camp-1-018-no-MacBook-Air.local>
2017-01-15 12:49:13 +0900

    Amazon APIを使用するために認証情報をenvironment.rbに記述

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,9 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Amazon::Ecs.options = {
+  associate_tag: 'xxx',
+  AWS_access_key_id: 'xxx',
+  AWS_secret_key: 'xxx'
+}
