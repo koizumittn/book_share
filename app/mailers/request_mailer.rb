ec7913b5ed8ec1847664b3f8f19981ec83fe32be
tech-camp <tech-camp@tech-camp-1-018-no-MacBook-Air.local>
2017-01-16 18:57:57 +0900

    メーラーの実装

--- app/mailers/request_mailer.rb
+++ app/mailers/request_mailer.rb
@@ -1,2 +1,9 @@
 class RequestMailer < ApplicationMailer
+
+  def request_email(requester, owner)
+    @requester = requester
+    @owner = owner
+    mail(to: @owner.email, subject: '貸し出しリクエスト')
+  end
+
 end
